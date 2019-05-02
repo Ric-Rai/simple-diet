ab5ec27671aa3b1bfe1e068bfdf7d010e6bf1cbb
ric-rai <48387692+Ric-Rai@users.noreply.github.com>
2019-05-02 15:23:29 +0300

    Add Heroku fixes

--- application/foods/views.py
+++ application/foods/views.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import render_template, jsonify
 from flask_login import login_required, current_user
 from sqlalchemy import or_
@@ -13,8 +15,12 @@ from application.foods.forms import FoodForm
 @app.route("/foods/view")
 @login_required
 def foods_view():
-    foods = Food.query.filter(or_(Food.account_id == current_user.id, Food.account_id.is_(None))) \
-        .order_by(Food.account_id.desc(), Food.name)
+    if os.environ.get("HEROKU"):
+        foods = Food.query.filter(or_(Food.account_id == current_user.id, Food.account_id.is_(None))) \
+            .order_by(Food.account_id.asc(), Food.name)
+    else:
+        foods = Food.query.filter(or_(Food.account_id == current_user.id, Food.account_id.is_(None))) \
+            .order_by(Food.account_id.desc(), Food.name)
     return render_template("components/table.html", rows=foods, css_class="foods", url="foods", form=FoodForm())
 
 
